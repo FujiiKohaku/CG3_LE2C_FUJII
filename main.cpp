cc35f84cbf8e1c76ac1b0432be54e5d3df3568cf
FujiiKohaku <k024g0078@g.neec.ac.jp>
2025-04-16 15:46:41 +0900

    ログだせたやりますねぇ

--- main.cpp
+++ main.cpp
@@ -1,5 +1,44 @@
 #include <Windows.h>
 #include <cstdint>
+#include <format>
+#include <string>
+// 関数の作成
+void Log(const std::string &message) { OutputDebugStringA(message.c_str()); }
+
+std::wstring ConvertString(const std::string &str) {
+  if (str.empty()) {
+    return std::wstring();
+  }
+
+  auto sizeNeeded =
+      MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char *>(&str[0]),
+                          static_cast<int>(str.size()), NULL, 0);
+  if (sizeNeeded == 0) {
+    return std::wstring();
+  }
+  std::wstring result(sizeNeeded, 0);
+  MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char *>(&str[0]),
+                      static_cast<int>(str.size()), &result[0], sizeNeeded);
+  return result;
+}
+
+std::string ConvertString(const std::wstring &str) {
+  if (str.empty()) {
+    return std::string();
+  }
+
+  auto sizeNeeded =
+      WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()),
+                          NULL, 0, NULL, NULL);
+  if (sizeNeeded == 0) {
+    return std::string();
+  }
+  std::string result(sizeNeeded, 0);
+  WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()),
+                      result.data(), sizeNeeded, NULL, NULL);
+  return result;
+}
+
 // ウィンドウプロシージャ
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
   // メッセージに応じて固有の処理を行う
@@ -67,7 +106,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
   }
 
   // 出力ウィンドウへの文字出力
-  OutputDebugStringA("Hello,DirectX!\n");
-
+  Log("HelloWorld\n");
+  Log(ConvertString(std::format(L"clientSize:{},{}\n", kClientWidth,kClientHeight)));
   return 0;
 }
