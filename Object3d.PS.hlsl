c32c84f764868ea59044424a668b5f2171ca145a
FujiiKohaku <k024g0078@g.neec.ac.jp>
2025-05-17 09:48:47 +0900

    hlslをいじったので今は色指定していないので 描画できなくてエラーが出ると思うけど安心したください

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -9,17 +9,12 @@ struct PixelShaderOutput
     float32_t4 color : SV_Target0;
 };
 
-struct PixelShaderOutput
-{
-    float32_t4 color : SV_TARGET0;
-};
 
 PixelShaderOutput main()
 {
     PixelShaderOutput output;
     output.color = gMaterial.color;
-    return
-output;
+    return output;
 
 
 }
