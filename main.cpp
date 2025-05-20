29621bc9ce70e7acbb38f97549529966cc546d65
FujiiKohaku <k024g0078@g.neec.ac.jp>
2025-05-20 20:17:19 +0900

    なんかビューポート通せたみたいです

--- main.cpp
+++ main.cpp
@@ -22,13 +22,31 @@
 struct Vector4 {
   float x, y, z, w;
 };
+struct Vector3 {
+  float x, y, z;
+};
 struct Matrix4x4 {
   float m[4][4];
 };
+struct Transform {
+  Vector3 scale;
+  Vector3 rotate;
+  Vector3 translate;
+};
+
+// 変数//
+
+// トランスフォーム
+Transform transform{{1.0f, 1.0f, 1.0f}, {0.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 0.0f}};
+// カメラトランスフォーム
+Transform cameraTransform{
+    {1.0f, 1.0f, 1.0f}, {0.0f, 0.0f, 0.0f}, {0.0f, 0.0f, -5.0f}};
+
+// 変数//
 //////////////
 // 関数の作成///
 //////////////
-
+#pragma region 行列関数
 // 単位行列の作成
 Matrix4x4 MakeIdentity4x4() {
   Matrix4x4 result{};
@@ -36,6 +54,239 @@ Matrix4x4 MakeIdentity4x4() {
     result.m[i][i] = 1.0f;
   return result;
 }
+// 拡大縮小行列S
+Matrix4x4 Matrix4x4MakeScaleMatrix(const Vector3 &s) {
+  Matrix4x4 result = {};
+  result.m[0][0] = s.x;
+  result.m[1][1] = s.y;
+  result.m[2][2] = s.z;
+  result.m[3][3] = 1.0f;
+  return result;
+}
+
+// X軸回転行列R
+Matrix4x4 MakeRotateXMatrix(float radian) {
+  Matrix4x4 result = {};
+
+  result.m[0][0] = 1.0f;
+  result.m[1][1] = std::cos(radian);
+  result.m[1][2] = std::sin(radian);
+  result.m[2][1] = -std::sin(radian);
+  result.m[2][2] = std::cos(radian);
+  result.m[3][3] = 1.0f;
+
+  return result;
+}
+// Y軸回転行列R
+Matrix4x4 MakeRotateYMatrix(float radian) {
+  Matrix4x4 result = {};
+
+  result.m[0][0] = std::cos(radian);
+  result.m[0][2] = std::sin(radian);
+  result.m[1][1] = 1.0f;
+  result.m[2][0] = -std::sin(radian);
+  result.m[2][2] = std::cos(radian);
+  result.m[3][3] = 1.0f;
+
+  return result;
+}
+// Z軸回転行列R
+Matrix4x4 MakeRotateZMatrix(float radian) {
+  Matrix4x4 result = {};
+
+  result.m[0][0] = std::cos(radian);
+  result.m[0][1] = -std::sin(radian);
+  result.m[1][0] = std::sin(radian);
+  result.m[1][1] = std::cos(radian);
+  result.m[2][2] = 1.0f;
+  result.m[3][3] = 1.0f;
+
+  return result;
+}
+
+// 平行移動行列T
+Matrix4x4 MakeTranslateMatrix(const Vector3 &tlanslate) {
+  Matrix4x4 result = {};
+  result.m[0][0] = 1.0f;
+  result.m[1][1] = 1.0f;
+  result.m[2][2] = 1.0f;
+  result.m[3][3] = 1.0f;
+  result.m[3][0] = tlanslate.x;
+  result.m[3][1] = tlanslate.y;
+  result.m[3][2] = tlanslate.z;
+
+  return result;
+}
+// 行列の積
+Matrix4x4 Multiply(const Matrix4x4 &m1, const Matrix4x4 &m2) {
+  Matrix4x4 result{};
+  for (int i = 0; i < 4; ++i)
+    for (int j = 0; j < 4; ++j)
+      for (int k = 0; k < 4; ++k)
+        result.m[i][j] += m1.m[i][k] * m2.m[k][j];
+  return result;
+}
+// ワールドマトリックス、メイクアフィン
+Matrix4x4 MakeAffineMatrix(const Vector3 &scale, const Vector3 &rotate,
+                           const Vector3 &translate) {
+  Matrix4x4 scaleMatrix = Matrix4x4MakeScaleMatrix(scale);
+  Matrix4x4 rotateX = MakeRotateXMatrix(rotate.x);
+  Matrix4x4 rotateY = MakeRotateYMatrix(rotate.y);
+  Matrix4x4 rotateZ = MakeRotateZMatrix(rotate.z);
+  Matrix4x4 rotateMatrix = Multiply(Multiply(rotateX, rotateY), rotateZ);
+  Matrix4x4 translateMatrix = MakeTranslateMatrix(translate);
+
+  Matrix4x4 worldMatrix =
+      Multiply(Multiply(scaleMatrix, rotateMatrix), translateMatrix);
+  return worldMatrix;
+}
+// 4x4 行列の逆行列を計算する関数
+Matrix4x4 Inverse(Matrix4x4 m) {
+  Matrix4x4 result;
+  float det;
+  int i;
+
+  result.m[0][0] =
+      m.m[1][1] * m.m[2][2] * m.m[3][3] - m.m[1][1] * m.m[2][3] * m.m[3][2] -
+      m.m[2][1] * m.m[1][2] * m.m[3][3] + m.m[2][1] * m.m[1][3] * m.m[3][2] +
+      m.m[3][1] * m.m[1][2] * m.m[2][3] - m.m[3][1] * m.m[1][3] * m.m[2][2];
+
+  result.m[0][1] =
+      -m.m[0][1] * m.m[2][2] * m.m[3][3] + m.m[0][1] * m.m[2][3] * m.m[3][2] +
+      m.m[2][1] * m.m[0][2] * m.m[3][3] - m.m[2][1] * m.m[0][3] * m.m[3][2] -
+      m.m[3][1] * m.m[0][2] * m.m[2][3] + m.m[3][1] * m.m[0][3] * m.m[2][2];
+
+  result.m[0][2] =
+      m.m[0][1] * m.m[1][2] * m.m[3][3] - m.m[0][1] * m.m[1][3] * m.m[3][2] -
+      m.m[1][1] * m.m[0][2] * m.m[3][3] + m.m[1][1] * m.m[0][3] * m.m[3][2] +
+      m.m[3][1] * m.m[0][2] * m.m[1][3] - m.m[3][1] * m.m[0][3] * m.m[1][2];
+
+  result.m[0][3] =
+      -m.m[0][1] * m.m[1][2] * m.m[2][3] + m.m[0][1] * m.m[1][3] * m.m[2][2] +
+      m.m[1][1] * m.m[0][2] * m.m[2][3] - m.m[1][1] * m.m[0][3] * m.m[2][2] -
+      m.m[2][1] * m.m[0][2] * m.m[1][3] + m.m[2][1] * m.m[0][3] * m.m[1][2];
+
+  result.m[1][0] =
+      -m.m[1][0] * m.m[2][2] * m.m[3][3] + m.m[1][0] * m.m[2][3] * m.m[3][2] +
+      m.m[2][0] * m.m[1][2] * m.m[3][3] - m.m[2][0] * m.m[1][3] * m.m[3][2] -
+      m.m[3][0] * m.m[1][2] * m.m[2][3] + m.m[3][0] * m.m[1][3] * m.m[2][2];
+
+  result.m[1][1] =
+      m.m[0][0] * m.m[2][2] * m.m[3][3] - m.m[0][0] * m.m[2][3] * m.m[3][2] -
+      m.m[2][0] * m.m[0][2] * m.m[3][3] + m.m[2][0] * m.m[0][3] * m.m[3][2] +
+      m.m[3][0] * m.m[0][2] * m.m[2][3] - m.m[3][0] * m.m[0][3] * m.m[2][2];
+
+  result.m[1][2] =
+      -m.m[0][0] * m.m[1][2] * m.m[3][3] + m.m[0][0] * m.m[1][3] * m.m[3][2] +
+      m.m[1][0] * m.m[0][2] * m.m[3][3] - m.m[1][0] * m.m[0][3] * m.m[3][2] -
+      m.m[3][0] * m.m[0][2] * m.m[1][3] + m.m[3][0] * m.m[0][3] * m.m[1][2];
+
+  result.m[1][3] =
+      m.m[0][0] * m.m[1][2] * m.m[2][3] - m.m[0][0] * m.m[1][3] * m.m[2][2] -
+      m.m[1][0] * m.m[0][2] * m.m[2][3] + m.m[1][0] * m.m[0][3] * m.m[2][2] +
+      m.m[2][0] * m.m[0][2] * m.m[1][3] - m.m[2][0] * m.m[0][3] * m.m[1][2];
+
+  result.m[2][0] =
+      m.m[1][0] * m.m[2][1] * m.m[3][3] - m.m[1][0] * m.m[2][3] * m.m[3][1] -
+      m.m[2][0] * m.m[1][1] * m.m[3][3] + m.m[2][0] * m.m[1][3] * m.m[3][1] +
+      m.m[3][0] * m.m[1][1] * m.m[2][3] - m.m[3][0] * m.m[1][3] * m.m[2][1];
+
+  result.m[2][1] =
+      -m.m[0][0] * m.m[2][1] * m.m[3][3] + m.m[0][0] * m.m[2][3] * m.m[3][1] +
+      m.m[2][0] * m.m[0][1] * m.m[3][3] - m.m[2][0] * m.m[0][3] * m.m[3][1] -
+      m.m[3][0] * m.m[0][1] * m.m[2][3] + m.m[3][0] * m.m[0][3] * m.m[2][1];
+
+  result.m[2][2] =
+      m.m[0][0] * m.m[1][1] * m.m[3][3] - m.m[0][0] * m.m[1][3] * m.m[3][1] -
+      m.m[1][0] * m.m[0][1] * m.m[3][3] + m.m[1][0] * m.m[0][3] * m.m[3][1] +
+      m.m[3][0] * m.m[0][1] * m.m[1][3] - m.m[3][0] * m.m[0][3] * m.m[1][1];
+
+  result.m[2][3] =
+      -m.m[0][0] * m.m[1][1] * m.m[2][3] + m.m[0][0] * m.m[1][3] * m.m[2][1] +
+      m.m[1][0] * m.m[0][1] * m.m[2][3] - m.m[1][0] * m.m[0][3] * m.m[2][1] -
+      m.m[2][0] * m.m[0][1] * m.m[1][3] + m.m[2][0] * m.m[0][3] * m.m[1][1];
+
+  result.m[3][0] =
+      -m.m[1][0] * m.m[2][1] * m.m[3][2] + m.m[1][0] * m.m[2][2] * m.m[3][1] +
+      m.m[2][0] * m.m[1][1] * m.m[3][2] - m.m[2][0] * m.m[1][2] * m.m[3][1] -
+      m.m[3][0] * m.m[1][1] * m.m[2][2] + m.m[3][0] * m.m[1][2] * m.m[2][1];
+
+  result.m[3][1] =
+      m.m[0][0] * m.m[2][1] * m.m[3][2] - m.m[0][0] * m.m[2][2] * m.m[3][1] -
+      m.m[2][0] * m.m[0][1] * m.m[3][2] + m.m[2][0] * m.m[0][2] * m.m[3][1] +
+      m.m[3][0] * m.m[0][1] * m.m[2][2] - m.m[3][0] * m.m[0][2] * m.m[2][1];
+
+  result.m[3][2] =
+      -m.m[0][0] * m.m[1][1] * m.m[3][2] + m.m[0][0] * m.m[1][2] * m.m[3][1] +
+      m.m[1][0] * m.m[0][1] * m.m[3][2] - m.m[1][0] * m.m[0][2] * m.m[3][1] -
+      m.m[3][0] * m.m[0][1] * m.m[1][2] + m.m[3][0] * m.m[0][2] * m.m[1][1];
+
+  result.m[3][3] =
+      m.m[0][0] * m.m[1][1] * m.m[2][2] - m.m[0][0] * m.m[1][2] * m.m[2][1] -
+      m.m[1][0] * m.m[0][1] * m.m[2][2] + m.m[1][0] * m.m[0][2] * m.m[2][1] +
+      m.m[2][0] * m.m[0][1] * m.m[1][2] - m.m[2][0] * m.m[0][2] * m.m[1][1];
+
+  det = m.m[0][0] * result.m[0][0] + m.m[0][1] * result.m[1][0] +
+        m.m[0][2] * result.m[2][0] + m.m[0][3] * result.m[3][0];
+
+  if (det == 0)
+    return Matrix4x4{}; // またはエラー処理
+
+  det = 1.0f / det;
+
+  for (i = 0; i < 4; i++)
+    for (int j = 0; j < 4; j++)
+      result.m[i][j] = result.m[i][j] * det;
+
+  return result;
+}
+// 透視投影行列
+Matrix4x4 MakePerspectiveFovMatrix(float fovY, float aspectRatio,
+                                   float nearClip, float farClip) {
+  Matrix4x4 result = {};
+
+  float f = 1.0f / std::tan(fovY / 2.0f);
+
+  result.m[0][0] = f / aspectRatio;
+  result.m[1][1] = f;
+  result.m[2][2] = farClip / (farClip - nearClip);
+  result.m[2][3] = 1.0f;
+  result.m[3][2] = (-nearClip * farClip) / (farClip - nearClip);
+
+  return result;
+}
+// 正射影行列
+Matrix4x4 MakeOrthographicMatrix(float left, float top, float right,
+                                 float bottom, float nearClip, float farClip) {
+  Matrix4x4 m = {};
+
+  m.m[0][0] = 2.0f / (right - left);
+  m.m[1][1] = 2.0f / (top - bottom);
+  m.m[2][2] = 1.0f / (farClip - nearClip);
+  m.m[3][0] = -(right + left) / (right - left);
+  m.m[3][1] = -(top + bottom) / (top - bottom);
+  m.m[3][2] = -nearClip / (farClip - nearClip);
+  m.m[3][3] = 1.0f;
+
+  return m;
+}
+// ビューポート変換行列
+Matrix4x4 MakeViewportMatrix(float left, float top, float width, float height,
+                             float minDepth, float maxDepth) {
+  Matrix4x4 m = {};
+
+  // 行0：X方向スケーリングと移動
+  m.m[0][0] = width / 2.0f;
+  m.m[3][0] = left + width / 2.0f;
+  m.m[1][1] = -height / 2.0f;
+  m.m[3][1] = top + height / 2.0f;
+  m.m[2][2] = maxDepth - minDepth;
+  m.m[2][3] = minDepth;
+  m.m[3][3] = 1.0f;
+
+  return m;
+}
+#pragma endregion
 
 static LONG WINAPI ExportDump(EXCEPTION_POINTERS *exception) {
   // 時刻を取得して、時刻を名前に入れたファイルを作成。Dumpsディレクトリ以下ぶ出力
@@ -673,11 +924,11 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
   // WVPリソースを作る02_02
   ID3D12Resource *wvpResource =
       CreateBufferRespource(device, sizeof(Matrix4x4));
-  // データを書き込む
+  // データを書き込む02_02
   Matrix4x4 *wvpData = nullptr;
-  // 書き込むためのアドレスを取得
+  // 書き込むためのアドレスを取得02_02
   wvpResource->Map(0, nullptr, reinterpret_cast<void **>(&wvpData));
-  // 単位行列を書き込んでおく
+  // 単位行列を書き込んでおく02_02
   *wvpData = MakeIdentity4x4(); // WVPリソースを作る
 
   MSG msg{};
@@ -692,6 +943,26 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
     } else {
 
       // ゲームの処理
+      // 02_02
+      transform.rotate.y += 0.03;
+
+      // メイクアフィンマトリックス02_02
+      Matrix4x4 worldMatrix = MakeAffineMatrix(
+          transform.scale, transform.rotate, transform.translate);
+      // カメラのメイクアフィンマトリックス02_02
+      Matrix4x4 cameraMatrix =
+          MakeAffineMatrix(cameraTransform.scale, cameraTransform.rotate,
+                           cameraTransform.translate);
+      // 逆行列カメラ02_02
+      Matrix4x4 viewMatrix = Inverse(cameraMatrix);
+      // 透視投影行列02_02
+      Matrix4x4 projectionMatrix = MakePerspectiveFovMatrix(
+          0.45f, float(kClientWidth) / float(kClientHeight), 0.1f, 100.0f);
+      // ワールドビュープロジェクション行列02_02
+      Matrix4x4 worldViewProjectionMatrix =
+          Multiply(worldMatrix, Multiply(viewMatrix, projectionMatrix));
+      // CBVのバッファに書き込む02_02
+      *wvpData = worldViewProjectionMatrix;
 
       // 画面のクリア処理
       //   これから書き込むバックバッファのインデックスを取得
