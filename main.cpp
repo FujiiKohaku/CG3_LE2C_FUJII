2a9e33a58df63cc5fbd79841e7320e024a0e8669
FujiiKohaku <k024g0078@g.neec.ac.jp>
2025-06-10 10:15:36 +0900

    k

--- main.cpp
+++ main.cpp
@@ -1224,37 +1224,12 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
   hr = device->CreateGraphicsPipelineState(
       &graphicsPipelineStateDesc, IID_PPV_ARGS(&graphicsPinelineState));
   assert(SUCCEEDED(hr));
-
   ID3D12Resource *vertexResource =
-      CreateBufferResource(device, sizeof(VertexData) * 6);
+      CreateBufferResource(device, sizeof(VertexData) * kNumVertices);
 
   // sprite用の頂点リソースを作る04_00
   ID3D12Resource *vertexResourceSprite =
-      CreateBufferResource(device, sizeof(VertexData) * 6);
-  //// 頂点リソース用のヒープの設定
-  // D3D12_HEAP_PROPERTIES uploadHeapProperties{};
-  // uploadHeapProperties.Type = D3D12_HEAP_TYPE_UPLOAD; // Uploadheapを使う
-  //// 頂点リソースの設定
-  // D3D12_RESOURCE_DESC vertexResourceDesc{};
-  //// バッファリソース。テクスチャの場合はまた別の設定をする
-  // vertexResourceDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
-  // vertexResourceDesc.Width = sizeof(Vector4) * 3; // リソースのサイズ　
-  //// バッファの場合はこれらは１にする決まり
-  // vertexResourceDesc.Height = 1;
-  // vertexResourceDesc.DepthOrArraySize = 1;
-  // vertexResourceDesc.MipLevels = 1;
-  // vertexResourceDesc.SampleDesc.Count = 1;
-  //// バッファの場合はこれにする決まり
-  // vertexResourceDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
-  // ID3D12Resource *vertexresource =
-  //     CreateBufferRespource(device, sizeof(Vector4) * 3);
-  //// 実際に頂点リソースを作る
-  // ID3D12Resource *vertexResource = nullptr;
-  // hr = device->CreateCommittedResource(
-  //     &uploadHeapProperties, D3D12_HEAP_FLAG_NONE, &vertexResourceDesc,
-  //     D3D12_RESOURCE_STATE_GENERIC_READ, nullptr,
-  //     IID_PPV_ARGS(&vertexResource));
-  // assert(SUCCEEDED(hr));
+      CreateBufferResource(device, sizeof(VertexData) * kNumVertices);
 
   // 03_01_Other
   ID3D12Resource *depthStencillResource =
