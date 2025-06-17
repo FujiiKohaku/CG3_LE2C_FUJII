89c0618045f3b6ae477415a90c64aae2f1a751c4
FujiiKohaku <k024g0078@g.neec.ac.jp>
2025-06-17 21:02:21 +0900

    s

--- main.cpp
+++ main.cpp
@@ -64,7 +64,7 @@ struct Material {
   Vector4 color;
   int32_t enableLighting;
 };
-struct TraansformationMatrix {
+struct TransformationMatrix {
   Matrix4x4 WVP;
   Matrix4x4 World;
 };
@@ -1408,13 +1408,17 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
   materialDataSprite->enableLighting = false; // kokomonstrball?
 
   // WVPリソースを作る02_02
-  ID3D12Resource *wvpResource = CreateBufferResource(device, sizeof(Matrix4x4));
+  ID3D12Resource *wvpResource =
+      CreateBufferResource(device, sizeof(TransformationMatrix));
   // データを書き込む02_02
-  Matrix4x4 *wvpData = nullptr;
+  TransformationMatrix *wvpData = nullptr;
   // 書き込むためのアドレスを取得02_02
   wvpResource->Map(0, nullptr, reinterpret_cast<void **>(&wvpData));
   // 単位行列を書き込んでおく02_02
-  *wvpData = MakeIdentity4x4(); // WVPリソースを作る
+  Matrix4x4 identity = MakeIdentity4x4();
+  // 05_03
+  memcpy(&wvpData->WVP, &identity, sizeof(Matrix4x4));
+  memcpy(&wvpData->World, &identity, sizeof(Matrix4x4));
 
   // sprite用のTransfomationMatrix用のリソースを作る。Matrix4x4
   // 1つ分のサイズを用意する04_00
