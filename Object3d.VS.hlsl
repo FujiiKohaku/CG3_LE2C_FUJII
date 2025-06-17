39e609c40cd3b9906f1e646021b833c7a82f63ea
FujiiKohaku <k024g0078@g.neec.ac.jp>
2025-06-17 21:37:43 +0900

    j

--- Object3d.VS.hlsl
+++ Object3d.VS.hlsl
@@ -13,6 +13,7 @@ struct VertexSgaderInput
 {
     float32_t4 position : POSITION0;
     float32_t2 texcoord : TEXCOORD0;
+    float32_t3 normal : NORMAL0;
 };
 
 
@@ -21,6 +22,7 @@ VertexShaderOutput main(VertexSgaderInput input)
     VertexShaderOutput output;
     output.position = mul(input.position, gTransformationMatrix.WVP);
     output.texcoord = input.texcoord;
+    output.normal = normalize(mul(input.normal, (float32_t3x3) gTransformationMatrix.World));
     return output;
 }
 
