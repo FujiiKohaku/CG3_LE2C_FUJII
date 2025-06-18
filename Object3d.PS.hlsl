c5e734e3ba959866fa59caf8d532b0f18bf78169
FujiiKohaku <k024g0078@g.neec.ac.jp>
2025-06-18 14:22:19 +0900

    あ

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -25,11 +25,11 @@ PixelShaderOutput main(VertexShaderOutput input)
     
     if (gMaterial.enableLighting != 0)//LightingÇ∑ÇÈèÍçá
     {
-        //float cos = saturate(dot(normalize(input.normal), -gDirectionalLight.direction));
-        //output.color = gMaterial.color * textureColor * gDirectionalLight.color * cos * gDirectionalLight.intensity;
+        float cos = saturate(dot(normalize(input.normal), -gDirectionalLight.direction));
+        output.color = gMaterial.color * textureColor * gDirectionalLight.color * cos * gDirectionalLight.intensity;
         //half lambert
-        float NdotL = dot(normalize(input.normal), -gDirectionalLight.direction);
-        float cos =  pow(NdotL * 0.5f + 0.5f, 2.0f);
+        //float NdotL = dot(normalize(input.normal), -gDirectionalLight.direction);
+        //float cos =  pow(NdotL * 0.5f + 0.5f, 2.0f);
         
         output.color = cos * gMaterial.color * textureColor;
         
