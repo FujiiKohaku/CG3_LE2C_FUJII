8697ad0e2d57ba50c64b40d52fb711b5edc4d62a
FujiiKohaku <k024g0078@g.neec.ac.jp>
2025-07-02 16:43:53 +0900

    j

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -45,13 +45,13 @@ PixelShaderOutput main(VertexShaderOutput input)
     // ───────────────────────────────────────
     //  コースティクス模様（時間＋UVで生成）
     // ───────────────────────────────────────
-float2 uv = transformedUV.xy * 10.0f;
+float2 uv = transformedUV.xy * 15.0f;
 
 // 複数方向から波を合成
 float wave =
     sin(uv.x * 6.0f + time * 2.5f) +
     sin(uv.y * 8.0f + time * 3.0f) +
-    sin((uv.x + uv.y) * 5.0f + time * 4.0f) +
+    sin((uv.x + uv.y) * 5.0f + time * 5.0f) +
     sin((uv.x - uv.y) * 7.0f - time * 3.5f);
 
 // 正規化＆強調
